e6091e9f86c88c26d8cf1ea72f7ddac54fd9a866
PrzemyslawSochon <115543375+PrzemyslawSochon@users.noreply.github.com>
2025-07-01 14:36:27 +0200

    fix: separate comment asterisks from its content

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values*/
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values */
 
 getRandomIntInclusive = (min, max) => {
   const minCeiled = Math.ceil(min);
