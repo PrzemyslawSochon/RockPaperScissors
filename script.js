25f6c94d44ba2ebf5fb02cb706da2a7998f26d30
PrzemyslawSochon <115543375+PrzemyslawSochon@users.noreply.github.com>
2025-07-01 14:34:31 +0200

    feat: add function returning random arbitrary int

---
+++ script.js
@@ -0,0 +1,7 @@
+/*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values*/
+
+getRandomIntInclusive = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
