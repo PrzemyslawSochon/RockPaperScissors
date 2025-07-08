f1dfe7afda8e80c80e0ccdca1b0c2b34362113ff
PrzemyslawSochon <115543375+PrzemyslawSochon@users.noreply.github.com>
2025-07-08 11:48:50 +0200

    fix: move score vars above the comment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values */
-
 let computerScore = 0;
 let humanScore = 0;
+
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values */
 
 getRandomIntInclusive = (min, max) => {
   const minCeiled = Math.ceil(min);
