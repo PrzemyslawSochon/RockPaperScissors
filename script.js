b11d877abd6ca0d556524edecfffa57e2e2b538c
PrzemyslawSochon <115543375+PrzemyslawSochon@users.noreply.github.com>
2025-07-08 10:57:59 +0200

    feat: add initial human and computer score vars

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values */
 
+let computerScore = 0;
+let humanScore = 0;
+
 getRandomIntInclusive = (min, max) => {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
